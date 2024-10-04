5512152eb26adf29355defe1965b2026fcfe2d6c
Ieon <yieon1028@gmail.com>
2024-10-04 20:21:35 +0900

    시작 준비

---
+++ move_character_to_hand.py
@@ -0,0 +1,8 @@
+from pico2d import *
+
+WIDTH, HEIGHT = 1280, 1024
+open_canvas(WIDTH, HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
