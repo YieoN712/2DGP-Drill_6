c0bbd34c9072028812b48ff6cda5828ae9073cc2
Ieon <yieon1028@gmail.com>
2024-10-04 20:27:45 +0900

    함수와 while문 추가
    랜덤으로 손을 생성하는 함수, 캐릭터가 이동하는 함수 추가

--- move_character_to_hand.py
+++ move_character_to_hand.py
@@ -6,3 +6,19 @@ open_canvas(WIDTH, HEIGHT)
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
+
+def rand_hand():
+    pass
+
+def move_character():
+    pass
+
+running = True
+x, y = WIDTH // 2, HEIGHT // 2
+frame = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(WIDTH // 2, HEIGHT // 2)
+    update_canvas()
